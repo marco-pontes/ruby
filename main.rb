6f810acf859c9d3b79af23a32bbd3cb12fd71da4
Marco Aurelio Pontes <marco.pontes@neogrid.com>
2017-10-13 11:57:15 -0300

    First tests, first class

--- main.rb
+++ main.rb
@@ -1,2 +1,8 @@
 class Main
+  10.times do print "teste \n" end
+  #print "2 + 3 is equal to " + 2 + 3
+  #must convert to string first
+  puts "2 + 3 is equal to: " + (2 + 3).to_s
+  puts "integer arithmetic results in integer: " + (10/3).to_s
+  puts "with a floating point: " + (10.0/3).to_s
 end
