cb3e437fc6e60d8e8dd206d029920873d2c43d01
Marco Aurelio Pontes <marco.pontes@neogrid.com>
2017-10-20 14:58:59 -0200

    Reorganizing and refactoring methods

--- main.rb
+++ main.rb
@@ -23,7 +23,23 @@ class Main
   end
 
   def main()
-    10.times do print "teste \n" end
+    basics()
+    #Chapter 3: Data Expressions and flow control
+    data_expressions_flow()
+    #Strings
+    strings()
+    #Arrays and lists
+    arrays()
+    #Hashes
+    hashes()
+    #flow control
+    flow_control()
+  end
+
+  def basics
+    10.times do
+      print "teste \n"
+    end
     #print "2 + 3 is equal to " + 2 + 3
     #must convert to string first
     puts "2 + 3 is equal to: " + (2 + 3).to_s
@@ -37,45 +53,55 @@ class Main
     puts (2.+(2))
     puts (2.class)
     puts (puts.class)
-    puts (Kernel.class.to_s  + Kernel.puts.class.to_s)
-    dog.say("I","want","food")
+    puts (Kernel.class.to_s + Kernel.puts.class.to_s)
+    dog.say("I", "want", "food")
     #String class
     puts "This is a test".class.to_s + "This is a test".length.to_s
     #method has to be defined before
     test()
     puts test.class
-    #Chapter 3: Data Expressions and flow control
+  end
+
+  def data_expressions_flow
     x2 = 3
     #4h = 4 invalidputs("you are old enough") unless age < 18
     _3aa = 2
     _start_underscore = 3
     age = rand(28)
-    puts("age " +  age.to_s)
+    puts("age " + age.to_s)
     age2 = rand(28)
-    puts("age2 " +  age2.to_s)
+    puts("age2 " + age2.to_s)
     #conditions
     puts("you are too young to enter") if age <= 12
     puts("you are old enough") unless age <= 18
     puts("you are a teenager") if age.between?(12, 17)
     puts("age <=> age2: " + (age <=> age2).to_s)
     #loops
-    5.times do puts("aaaa") end
-    5.times do |number| puts number end
+    5.times do
+      puts("aaaa")
+    end
     5.times do |number|
       puts number
     end
-    5.times do |number| test end
-    5.times { puts("test curly") }
+    5.times do |number|
+      puts number
+    end
+    5.times do |number|
+      test
+    end
+    5.times {puts("test curly")}
     #if true {test()}
     5.upto(10) {|number| puts number}
     5.downto(0) {|number| puts number}
     0.step(50, 5) {|number| puts number}
-    receiveFunc{|param| puts "aaaa Works " + param}
+    receiveFunc {|param| puts "aaaa Works " + param}
     divide(10, 3)
     divideIntegers(10.0, 3.0)
+    #constant
     puts Pi
-    #Strings
+  end
 
+  def strings
     s = %q(
       test'
       multi line"
@@ -116,20 +142,22 @@ STRING_TEST
     puts "testtest".sub(/\w{1}/, "tasti")
     puts "testtest".sub(/^\w/, "tasti")
     puts "testtest".sub(/\w$/, "tasty")
-    "testtest".scan(/./){|letter| puts letter}
-    "this is a test".scan(/\w\w/){|letter| puts letter}
-    "this is a 10 test out of 1000".scan(/\d+/){|digits| puts digits}
-    "this is a test".scan(/[aeiou]/){|letter| puts letter}
+    "testtest".scan(/./) {|letter| puts letter}
+    "this is a test".scan(/\w\w/) {|letter| puts letter}
+    "this is a 10 test out of 1000".scan(/\d+/) {|digits| puts digits}
+    "this is a test".scan(/[aeiou]/) {|letter| puts letter}
     puts ""
-    "this is a test".scan(/[a-o]/){|letter| puts letter}
+    "this is a test".scan(/[a-o]/) {|letter| puts letter}
     puts "match position: " + ("this is a test" =~ /[a]/).to_s
     puts "true?" if ("this is a test" =~ /[p]/)
-    puts "ruby considers this as true?"  if ("this is a test" =~ /[t]/)
+    puts "ruby considers this as true?" if ("this is a test" =~ /[t]/)
     x = "This is a test".match(/(\w+)(\w+)/)
     puts x[0]
     puts x[1]
     puts x[2]
-    #Arrays and lists
+  end
+
+  def arrays
     array = [1, 2, 3, 4, 5]
     puts array[0]
     array[0] += 1
@@ -154,7 +182,7 @@ STRING_TEST
     array = array.map() {|element| element * 2}
     puts array.inspect
     i = 0
-    while(i < array.length)
+    while (i < array.length)
       puts array[i]
       i += 1
     end
@@ -170,20 +198,30 @@ STRING_TEST
     puts array2.last()
     puts array2.last(2).to_s
     puts array2.reverse.to_s
-    #Hashes
+  end
+
+  def hashes
     dict = {2 => 3, 3 => 4, 11 => 10}
     puts dict[2]
     dict2 = {"name" => "Marco", "age" => 29, "gender" => "Masculino"}
     puts dict2["name"]
     #dict3 = dict. dict2
     #p dict3.size
-    dict2.each{|key, value| puts "key: #{key}, value: #{value}"}
+    dict2.each {|key, value| puts "key: #{key}, value: #{value}"}
     puts dict2.keys
     puts dict2.values
     dict2.delete("name")
     puts dict2
-    dict.delete_if(){|key, value| value < 10}
+    dict.delete_if() {|key, value| value < 10}
     puts dict
+    users = {'Marco' => {'name' => 'Marco', 'email' => 'aurelio.pontes@gmail.com', 'emails' => ['email 1', 'email 2', 'email 3']}, 'Tamiris' => {'name' => 'Tamiris', 'email' => 'tamiris@gmail.com', 'emails' => ['email 4', 'email 5', 'email 6']}}
+    puts users
+    puts users['Marco']
+    puts users['Tamiris']
+  end
+
+  def flow_control()
+
   end
 end
 
